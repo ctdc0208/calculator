1857cce25faef512960c0b66624f79b04ac9c8f2
nexus0208 <christiandelacruz0208@gmail.com>
2020-08-12 09:27:49 +0800

    substract

--- calculator.js
+++ calculator.js
@@ -4,8 +4,9 @@ function add (num1, num2) {
 	return result
 }
 
-function subtract () {
-	result = a - b;
+function subtract (num1, num2) {
+	let result = num1 - num2
+	return result
 }
 
 function sum () {
