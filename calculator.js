1018c8fe59dcbcd783b26ac645b5d5830e202ed0
nexus0208 <christiandelacruz0208@gmail.com>
2020-08-12 09:17:10 +0800

    addition

--- calculator.js
+++ calculator.js
@@ -1,5 +1,14 @@
 function add () {
-
+	  {
+	      var textInputval=0;
+	      var textInputval1=2;
+	      var temp=0;
+	      textInputval = parseInt(document.getElementById('new').value);
+	      textInputval1 = parseInt(document.getElementById('new').value);
+	      temp = textInputval + textInputval1;
+	      console.log(textInputval);
+	      console.log(textInputval1);
+	  }
 }
 
 function subtract () {
